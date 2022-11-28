49951b39cc6f0a41e540780ff31bf35e8734627e
Amar2050 <amarboutayeb@gmail.com>
2022-11-28 15:45:09 +0100

    utilisation d'un objet dans un echo

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
 
 $ballonFoot= new stdClass();
 $ballonFoot->type = "Football";
+$ballonFoot->marque = "Nike";
+$ballonFoot->taille = 5;
+$ballonFoot->status = "crevé";
 
+// var_dump($ballonFoot);
 
-var_dump($ballonFoot);
-
+echo "- Hey le ballon de $ballonFoot->type, $ballonFoot->marque est $ballonFoot->status, Ismaël ! <br><br>
+    - C'était un taille $ballonFoot->taille, Isaac c'est ça ? ";
