9921c9bdf8216c8e6df4b78aa08191d75d2c0553
Amar2050 <amarboutayeb@gmail.com>
2022-11-28 16:12:40 +0100

    Class Natives PHP

--- index.php
+++ index.php
@@ -9,7 +9,4 @@ $ballonFoot->marque = "Nike";
 $ballonFoot->taille = 5;
 $ballonFoot->status = "crevé";
 
-if(!property_exists($ballonFoot, "prix")){
-    $ballonFoot->prix = 5;
-    echo "Le ballon coute $ballonFoot->prix €";
-}
+var_dump(get_declared_classes());
