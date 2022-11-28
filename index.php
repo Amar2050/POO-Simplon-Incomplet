7b47c3fc1bcbffd45383f182f8941b7f55040277
Amar2050 <amarboutayeb@gmail.com>
2022-11-28 15:06:40 +0100

    Création objet via CONVERSION
    Via le var_dump l'on voit bien que l'array est devenue tableau

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
+//Création d'objet via CONVERSION
+// Conversion d'un tableau en objet 
 
+$tab["couleur"] = "bleu";
+$tab["type"] = "plume";
+$tab["prix"] = 5;
+
+$stylo = (object) $tab;
+
+var_dump($stylo);
